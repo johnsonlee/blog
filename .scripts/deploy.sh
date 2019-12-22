8af7c4331fae3fa46bf98f70401102abaa2bc263
johnsonlee <g.johnsonlee@gmail.com>
2019-12-22 15:49:30 +0800

    Update trivis config

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-git -C public add . \
+echo "johnsonlee.io" > public/CNAME \
+    && git -C public add . \
     && git -C public commit -m "Travis build: $TRAVIS_BUILD_NUMBER" -a \
-    && git -C public push -u origin HEAD:master
+    && git -C public push -u origin HEAD:master --force
