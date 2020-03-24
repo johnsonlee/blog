6003730761574c08ec3ddba99ee6a0a455bfe6c6
johnsonlee <g.johnsonlee@gmail.com>
2020-03-24 22:45:10 +0800

    fix: travis CI deploy error

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -2,8 +2,7 @@
 
 set -x
 
-npx hexo gzip \
-    && echo "johnsonlee.io" > public/CNAME \
+echo "johnsonlee.io" > public/CNAME \
     && git -C public add . \
     && git -C public commit -m "Travis build: $TRAVIS_BUILD_NUMBER" -a \
     && git -C public push -u origin HEAD:master --force
