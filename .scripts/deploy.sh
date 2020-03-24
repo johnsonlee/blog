7353c8420f2ed6a87cffdb3688247fa98660f295
johnsonlee <g.johnsonlee@gmail.com>
2020-03-24 23:26:58 +0800

    fix: travis CI deploy error

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -4,6 +4,8 @@ set -x
 
 cd public \
     && echo "johnsonlee.io" > CNAME \
+    && git init \
+    && git remote add origin https://${GH_TOKEN}@github.com/johnsonlee/johnsonlee.github.io.git \
     && git add . \
     && git status -s \
     && git commit -m "Travis build: $TRAVIS_BUILD_NUMBER" -a \
