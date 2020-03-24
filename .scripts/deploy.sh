bca43a6f5d3bc62d4e08882926811ba452c380a9
johnsonlee <g.johnsonlee@gmail.com>
2020-03-24 23:22:42 +0800

    fix: travis CI deploy error

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -2,11 +2,9 @@
 
 set -x
 
-pwd
-
-find . \( -path ./.git -o -path ./node_modules \) -prune -o -print
-
-echo "johnsonlee.io" > public/CNAME \
-    && git -C public add . \
-    && git -C public commit -m "Travis build: $TRAVIS_BUILD_NUMBER" -a \
-    && git -C public push -u origin HEAD:master --force
+cd public \
+    && echo "johnsonlee.io" > CNAME \
+    && git add . \
+    && git status -s \
+    && git commit -m "Travis build: $TRAVIS_BUILD_NUMBER" -a \
+    && git push -u origin HEAD:master --force
